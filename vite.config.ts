a60042e8c2fed9efeab8d1542a7b6b33a9c76cfe
Megahedron69 <karticjoshi68@gmail.com>
2024-09-09 17:40:26 +0530

    ...........

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/personal-porto",
+  base: "./personal-porto",
   plugins: [react()],
   build: {
     target: "ES2022",

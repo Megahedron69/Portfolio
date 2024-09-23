2b44a91a5f0a9b0adc61841f9ec19c0ef3ccf64c
Megahedron69 <karticjoshi68@gmail.com>
2024-09-23 16:38:27 +0530

    ( -_•)▄︻テحكـ━一

--- post-build.sh
+++ post-build.sh
@@ -2,16 +2,12 @@
 
 dest="dist/netlify.toml"
 
-# Ensure space after [ and before ]
 if [ ! -d dist ]; then
   echo "Dist folder does not exist."
   exit 1
 fi
 
-# Properly format the here-document and make sure the EOL is on a separate line
 cat <<EOL > "$dest"
-[build]
-  publish = "dist"
 [[redirects]]
   from = "/*"
   to = "/index.html"
